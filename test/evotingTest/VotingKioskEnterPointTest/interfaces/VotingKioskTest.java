3ce04b856c70cc81e924cf5cf9920b1d19c9e0da
David <damusa2003@gmail.com>
2023-12-29 11:42:31 +0100

    Structure for the test that check the order of the method calls

---
+++ test/evotingTest/VotingKioskEnterPointTest/interfaces/VotingKioskTest.java
@@ -0,0 +1,31 @@
+package evotingTest.VotingKioskEnterPointTest.interfaces;
+
+import data.Nif;
+import data.Password;
+import data.VotingOption;
+import exceptions.InvalidAccountException;
+import exceptions.InvalidDNIDocumException;
+import exceptions.NotEnabledException;
+import exceptions.ProceduralException;
+
+import java.net.ConnectException;
+
+public interface VotingKioskTest {
+    void setDocument(char c) throws ProceduralException;
+
+    void enterAccount(String login, Password pssw) throws InvalidAccountException, ProceduralException;
+
+
+    void confirmIdentif(char conf) throws InvalidDNIDocumException, ProceduralException;
+
+    void enterNif(Nif nif) throws NotEnabledException, ConnectException, ProceduralException;
+
+    void initOptionsNavigation() throws ProceduralException;
+
+
+    void consultVotingOption(VotingOption vopt) throws ProceduralException;
+
+    void vote() throws ProceduralException;
+
+    void confirmVotingOption(char conf) throws ProceduralException, ConnectException;
+}
